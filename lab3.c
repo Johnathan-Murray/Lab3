9540cedacaf6b1e2678cd55e4a13b9a1d6919916
Johnny Murray <jmurray17@ewu.edu>
2020-10-07 02:18:07 +0000

    put in formula and display functions

--- lab3.c
+++ lab3.c
@@ -15,40 +15,53 @@ double readWinningTime()
 
 double calcMetersPerSecond(const double DISTANCE, double winningTime)
 {
-  return 0.00;
+  return 100.0/winningTime;
 }
 
 double calcFeetPerSecond(double winningTime)
 {
-  return 0.00;
+  return (100.0*3.28084)/winningTime;
 }
 
 double calculateKillmeterPerHour(const double DISTANCE, double winningTime, double seconds)
 {
-  return 0.00;
+  return (100.0/1000.0)/((winningTime/60.0)/60.0);
 }
 
 double calcMilesPerHour(double winningTime, double mtomi)
 {
-  return 0.00;
+  return (100.0*0.00062137121212121)/((winningTime/60.0)/60.0);
 }
 
 void displayResults(double fps, double kph, double mph, double mps)
 {
-
+printf("\nThe person was traveling at a rate of:\n");					  
+      printf("%.2lf  meters per second.\n", mps);
+      printf("%.2lf feet per second.\n", fps);
+      printf("%.2lf miles per hour.\n", mph);
+      printf("%.2lf kilometers per hour.\n \n", kph);
 }
 
 void displayTimeToRunMile(double mph)
 {
+double mileTimeD = (1.0/mph)*60.0;
+      int mileTimeI    = (1.0/mph)*60.0;
 
+      printf("It would take the runner ");
+      printf("%d minutes ", mileTimeI);
+      printf("and ");
+      printf("%.2lf seconds ", mileTimeD);
+      printf("to run a mile. \n");
 }
 
 double calcTime100Yards(double fps)
 {
-  return 0.00;
+  return 300.0/fps;
 }
 
 void displayHundredYardsTime(double hundredYards)
 {
-  
+printf("It would take the runner ");
+      printf("%.2lf seconds ", hundredYards);
+      printf("to run 100 yards.");
 }
